f27dc7c3bf3b942ec7ab70d727edd8946818d103
Antonio Tangarife <linker@planetabinario.com>
2016-04-08 20:56:31 -0400

    Setting Screens

---
+++ LiveChair/BarberAccountTableViewController.swift
@@ -0,0 +1,111 @@
+//
+//  BarberAccountTableViewController.swift
+//  LiveChair
+//
+//  Created by Antonio Tangarife on 4/8/16.
+//  Copyright © 2016 LiveChair. All rights reserved.
+//
+
+import UIKit
+
+class BarberAccountTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+   /* override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 2
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 1
+    }*/
+    
+    
+    /*override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("logOutCell", forIndexPath: indexPath)
+
+        cell.targetForAction(#selector(BarberAccountTableViewController.logOut), withSender: self)
+
+        return cell
+    }*/
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        
+        // Logout
+        if (indexPath.section == 1)
+        {
+            //navigationController!.popViewControllerAnimated(true)
+            self.parentViewController!.dismissViewControllerAnimated(true, completion: {
+            })
+            //navigationController!.popViewControllerAnimated(true)
+            //self.performSegueWithIdentifier("barberHomeSegue", sender: self)
+//            navigationController!.popViewControllerAnimated(true)
+            
+        }
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- LiveChair/BarberHomeViewController.swift
+++ LiveChair/BarberHomeViewController.swift
@@ -14,6 +14,8 @@ class BarberHomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+
     }
     
     override func didReceiveMemoryWarning() {
@@ -36,5 +38,7 @@ class BarberHomeViewController: UIViewController {
         if let shopName = defaults.stringForKey("shopName") {
             self.navigationItem.title = shopName
         }
+        
     }
+    
 }
